cd7a02897501a6497f5338eb0f16ef3398308d67
Douglas Hartmann Liotto <douglas.liotto@dwanke.local>
2020-07-22 13:40:22 -0300

    Criando carousel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useEffect, useState, useRef } from 'react';
+import Slider from "react-slick";
+import Iframe from 'react-iframe';
+import localIpUrl from 'local-ip-url';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//https://react-slick.neostack.com/docs/example/dynamic-slides
+//https://www.npmjs.com/package/react-iframe
+
+const App = () => {
+    const slider = useRef();
+
+    useEffect(() => {
+      console.log(localIpUrl('public', 'ipv4'));
+
+    },[]);
+
+    const [ip, setIp] = useState();
+ 
+  
+  function gotTo(){
+    slider.slickGoTo(2);
+  }
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    lazyLoad: true
+  };
+
+    return (
+      <>
+        <div>
+          <h2> Single Item</h2>
+          <Slider ref={slider} {...settings}>
+            <div>
+              <Iframe url="https://blog.2amgaming.com/2019/11/otimizar-pc-csgo-maximo-fps/"
+                      width="100%"
+                      height="500px"
+                      id="myId"
+                      className="myClassname"
+                      display="initial"
+                      position="relative"/>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+        </div>
+      </>
+    );
 }
 
 export default App;
